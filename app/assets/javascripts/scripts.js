970410fddea076fd96995e4141e5693b0231e9d1
Tiago Martins <tiagom87@gmailc.om>
2014-09-24 21:44:21 -0700

    Fixed js

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -33,18 +33,7 @@
     #PRELOAD IMAGES
 \*------------------------------------*/
 
-
-
-
-
 $(document).ready(function(){
-$.preloadImages = function() {
-  for (var i = 0; i < arguments.length; i++) {
-    $("<img />").attr("src", arguments[i]);
-  }
-}
-
-$.preloadImages("monthmentor_logo_white.png","monthmentor_logo.png");
 
 
 /*------------------------------------*\
